55532cfdcc962c6dcea49ca73e5e92ee1bc29583
Joaquín Eduardo Piersigilli <joaco.piersigilli@hotmail.com>
2022-04-04 21:49:53 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,52 @@
-import { Header } from "@mantine/core";
+import styled from "styled-components";
 
 const HeaderComponent = () => {
   return (
-    <Header height={60} padding="sm" style={{ backgroundColor: "#00152f" }}>
+    <HeaderWrapper>
       <img
         src="https://constellationnetwork.io/wp-content/uploads/2021/04/Constellation-Logo_White.png"
-        width="150"
+        width="auto"
+        height="auto"
         alt="Constellation Logo"
       />
-    </Header>
+      <Badge>
+        <EthAmount>3.9913 ETH</EthAmount>
+      </Badge>
+    </HeaderWrapper>
   );
 };
+
+const Badge = styled.div`
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(218, 165, 32, 0.2);
+  border-radius: 10px;
+  font-weight: 600;
+`;
+
+const EthAmount = styled.h5`
+  margin: 0;
+  padding: 0;
+  color: gold;
+`;
+
+const HeaderWrapper = styled.div`
+  height: 60px;
+  display: block;
+  z-index: 100;
+  position: static;
+  box-sizing: border-box;
+  border-bottom: 1px solid #e9ecef;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica,
+    Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
+  -webkit-font-smoothing: antialiased;
+  padding: 16px;
+  display: flex;
+  background-color: #00152f;
+
+  justify-content: space-between;
+`;
 
 export default HeaderComponent;
